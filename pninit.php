0ef0eab113f267d3aac694cbf1b2fe217988d340
quan <quan@4f3a7d92-2849-42f7-b543-b8a74bcde5e1>
2010-02-28 16:12:03 +0000

    importing code base

---
+++ pninit.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @package      lobby
+ * @version      $Id $
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2009
+ * @license      no public license - read license.txt in doc directory for details
+ */
+
+ /**
+  * initialise the module
+  *
+  */
+function lobby_init() {
+  	// Create tables
+  	$tables = array (
+  		'lobby_pages',
+  		'lobby_groups',
+  		'lobby_categories',
+  		'lobby_moderators',
+  		'lobby_members',
+  		'lobby_members_pending',
+  		'lobby_invitations',
+  		'lobby_news',
+  		'lobby_users',
+  		'lobby_forums',
+	  	'lobby_forum_subscriptions',
+	  	'lobby_forum_topics',
+	  	'lobby_forum_topic_subscriptions',
+	  	'lobby_forum_posts',
+	  	'lobby_forum_posts_text',
+		  );
+	foreach ($tables as $table) {
+		if (!DBUtil::createTable($table)) {
+		  	return false;
+		}
+	}
+	// Set initial values for module variables
+	pnModSetVar('lobby','postsperpage',10);
+	pnModSetVar('lobby','topicsperpage',15);
+	pnModSetVar('lobby','newsperpage',15);
+	return true;
+}
+
+/**
+ * upgrade routine
+ */
+
+function lobby_upgrade($oldversion)
+{
+   switch($oldversion) {
+     case '1.0':
+     case '1.1':
+   	  	if (!DBUtil::changeTable('lobby_groups')) return false;
+    }
+    return true;
+}
+ /**
+  * initialise the module
+  *
+  */
+function lobby_delete() {
+  	// Drop tables
+  	$tables = array (
+  		'lobby_pages',
+  		'lobby_groups',
+  		'lobby_categories',
+  		'lobby_moderators',
+  		'lobby_members',
+  		'lobby_members_pending',
+  		'lobby_invitations',
+  		'lobby_news',
+  		'lobby_users',
+  		'lobby_forums',
+	  	'lobby_forum_subscriptions',
+	  	'lobby_forum_topics',
+	  	'lobby_forum_topic_subscriptions',
+	  	'lobby_forum_posts',
+	  	'lobby_forum_posts_text',
+		  );
+	foreach ($tables as $table) {
+		if (!DBUtil::dropTable($table)) {
+		  	return false;
+		}
+	}
+	// Delete module variables
+	pnModDelVar('lobby');
+	return true;
+}
